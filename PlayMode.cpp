d909d3916d76c707006b6b53ab52bd85df0065ca
keche1234 <156265488+keche1234@users.noreply.github.com>
2025-09-09 10:24:13 -0400

    Showing score, adding pnct files to git

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -1239,12 +1239,12 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 		));
 
 		constexpr float H = 0.09f;
-		lines.draw_text("W to accelerate, A/D to turn, Space to Jump",
+		lines.draw_text("W to accelerate, A/D to turn, Space to Jump ~ " + std::to_string(int(player->score)),
 			glm::vec3(-aspect + 0.1f * H, -1.0 + 0.1f * H, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0x00, 0x00, 0x00, 0x00));
 		float ofs = 2.0f / drawable_size.y;
-		lines.draw_text("W to accelerate, A/D to turn, Space to Jump",
+		lines.draw_text("W to accelerate, A/D to turn, Space to Jump ~ " + std::to_string(int(player->score)),
 			glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + 0.1f * H + ofs, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
